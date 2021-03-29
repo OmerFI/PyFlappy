0ef3cba92f7326c8dce26d393f58243f3ce870a0
OmerFI <54645034+OmerFI@users.noreply.github.com>
2021-03-29 04:23:57 -0400

    Add files via upload

---
+++ Level.py
@@ -0,0 +1,74 @@
+
+class Level1():
+    def __init__(self):
+        self.level = 1
+        self.boundary1x, self.boundary1y = (215, -542.4)
+        self.boundary2x, self.boundary2y = (215, 496.75)
+        self.boundary3x, self.boundary3y = (415, -502.8)
+        self.boundary4x, self.boundary4y = (415, 398.8)
+        self.boundary5x, self.boundary5y = (615, -405.45)
+        self.boundary6x, self.boundary6y = (615, 504.46)
+        self.boundary7x, self.boundary7y = (815, -447.6)
+        self.boundary8x, self.boundary8y = (815, 470.7)
+        self.boundary9x, self.boundary9y = (1015, -558.3)
+        self.boundary10x, self. boundary10y = (1015, 320)
+
+
+class Level2():
+    def __init__(self):
+        self.level = 2
+        self.boundary1x, self.boundary1y = (215, -396)
+        self.boundary2x, self.boundary2y = (215, 525.76)
+        self.boundary3x, self.boundary3y = (415, -502.8)
+        self.boundary4x, self.boundary4y = (415, 398.8)
+        self.boundary5x, self.boundary5y = (615, -405.45)
+        self.boundary6x, self.boundary6y = (615, 504.45)
+        self.boundary7x, self.boundary7y = (815, -492.6)
+        self.boundary8x, self.boundary8y = (815, 406.8)
+        self.boundary9x, self.boundary9y = (1015, -382.94)
+        self.boundary10x, self. boundary10y = (1015, 523.67)
+
+
+class Level3():
+    def __init__(self):
+        self.level = 3
+        self.boundary1x, self.boundary1y = (215, -463.63)
+        self.boundary2x, self.boundary2y = (215, 427.45)
+        self.boundary3x, self.boundary3y = (415, -628.6)
+        self.boundary4x, self.boundary4y = (415, 273.45)
+        self.boundary5x, self.boundary5y = (615, -402.21)
+        self.boundary6x, self.boundary6y = (615, 504.45)
+        self.boundary7x, self.boundary7y = (815, -316.58)
+        self.boundary8x, self.boundary8y = (815, 551.1)
+        self.boundary9x, self.boundary9y = (1015, -519.14)
+        self.boundary10x, self. boundary10y = (1015, 381.36)
+
+
+class Level4():
+    def __init__(self):
+        self.level = 4
+        self.boundary1x, self.boundary1y = (215, -504.77)
+        self.boundary2x, self.boundary2y = (215, 427)
+        self.boundary3x, self.boundary3y = (415, -200.4)
+        self.boundary4x, self.boundary4y = (415, 635.63)
+        self.boundary5x, self.boundary5y = (615, -327.29)
+        self.boundary6x, self.boundary6y = (615, 504.45)
+        self.boundary7x, self.boundary7y = (815, -558.4)
+        self.boundary8x, self.boundary8y = (815, 336.73)
+        self.boundary9x, self.boundary9y = (1015, -373)
+        self.boundary10x, self. boundary10y = (1015, 499.6)
+
+
+class Level5():
+    def __init__(self):
+        self.level = 5
+        self.boundary1x, self.boundary1y = (215, -510)
+        self.boundary2x, self.boundary2y = (215, 510)
+        self.boundary3x, self.boundary3y = (415, -564)
+        self.boundary4x, self.boundary4y = (415, 295)
+        self.boundary5x, self.boundary5y = (615, -312)
+        self.boundary6x, self.boundary6y = (615, 544)
+        self.boundary7x, self.boundary7y = (815, -331)
+        self.boundary8x, self.boundary8y = (815, 506)
+        self.boundary9x, self.boundary9y = (1015, -544)
+        self.boundary10x, self. boundary10y = (1015, 290)
